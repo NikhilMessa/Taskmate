7048ab451fa8fd13d01fe43e97c6d090b4fe920d
Nikhil Messa <messanikhil03@gmail.com>
2021-12-13 20:03:03 +0530

    Project updated

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import os
 from pathlib import Path
 import environ
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env=environ.Env(SECRET_KEY = str,)
@@ -133,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'todolist'
 LOGIN_URL = "login"
+
+django_heroku.settings(locals())
